d4aadeb2c8633b0ae286d6e598c017dfbd7eebb5
Devansh Dangi <dangi.devansh.0830@gmail.com>
2026-09-04 12:48:26 +0530

    Add files via upload

---
+++ DSA/Data-Structure-Algorithm/DSA/Data-Structure-Algorithm/classical_for_loop.java
@@ -0,0 +1,9 @@
+public class classical_for_loop {
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,5,6,7,8,9};
+        
+        for(int val : arr){
+            System.out.println(val);
+        }
+    }
+}
