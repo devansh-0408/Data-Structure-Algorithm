694e3b9aca77c23934d4f79131d7eb7d6e8d287d
Devansh Dangi <dangi.devansh.0830@gmail.com>
2026-09-04 12:32:56 +0530

    Add files via upload

---
+++ DSA/Data-Structure-Algorithm/table.java
@@ -0,0 +1,9 @@
+public class table {
+    public static void main(String[] args){
+        for(int i=1; i<=10; i++){
+            int ans=2*i;
+            System.out.println(ans);
+        }    
+    }
+    
+}
