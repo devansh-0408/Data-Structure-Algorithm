c777baef939b9f0d3417b9ea055504a0b42a632e
Devansh Dangi <dangi.devansh.0830@gmail.com>
2026-09-23 09:38:55 +0530

    Add files via upload

---
+++ DSA/Data-Structure-Algorithm/DSA/Data-Structure-Algorithm/twoD_array.java
@@ -0,0 +1,17 @@
+public class twoD_array {
+    public static void main(String[] args){
+    //     //declaration
+    //     int[][] arr;
+    //     //allocation
+    //     arr = new int[3][4];
+
+        //initialization
+        int[][] brr = {
+            {1,2},
+            {5,6},
+            {9,10},
+            {13,14}
+        };
+        System.out.println(brr[0][0]);
+    }
+}
